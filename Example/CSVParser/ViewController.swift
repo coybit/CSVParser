50c2d72b93719a9463a2ce58a9a74966e3e741d1
Coybit <coybit@gmail.com>
2017-03-24 19:07:36 -0700

    Makes it a Cocoapod

---
+++ Example/CSVParser/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  CSVParser
+//
+//  Created by coybit on 03/24/2017.
+//  Copyright (c) 2017 coybit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
--- Tests/LinuxMain.swift
+++
@@ -1,6 +0,0 @@
-import XCTest
-@testable import CSVParserTests
-
-XCTMain([
-     testCase(CSVParserTests.allTests),
-])
